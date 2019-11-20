9efdc13b9c68b8077e7e93e2e7224fccabc68785
Sasha Belova <s2288b@gmail.com>
2019-11-20 10:12:45 -0500

    switch statement replace

--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -1,25 +1,24 @@
 import React, { Component } from 'react';
 
 class Repository extends Component {
-  renderLanguage() {
-    let lang;
-    if (this.props.language === 'Python') {
-      lang = <span className="lang-python">{this.props.language}</span>;
-    } else if (this.props.language === 'PHP') {
-      lang = <span className="lang-php">{this.props.language}</span>;
-    } else if (this.props.language === 'HTML') {
-      lang = <span className="lang-html">{this.props.language}</span>;
-    } else if (this.props.language === 'Ruby') {
-      lang = <span className="lang-ruby">{this.props.language}</span>;
-    } else {
-      lang = <span className="lang-other">{this.props.language}</span>;
+  renderLanguage(language) {
+    switch (language) {
+      case 'Python':
+        return <span className="lang-python">{language}</span>;
+      case 'PHP':
+        return <span className="lang-php">{language}</span>;
+      case 'HTML':
+        return <span className="lang-php">{language}</span>;
+      case 'Ruby':
+        return <span className="lang-ruby">{language}</span>;
+      default:
+        return <span className="lang-other">{language}</span>;
     }
-    return lang;
   }
 
-  // getDateUpdated = (date) => {
-  //   return new Date(date).toUTCString();
-  // };
+  renderDate = date => {
+    return new Date(date).toUTCString();
+  };
 
   render() {
     return (
@@ -30,11 +29,12 @@ class Repository extends Component {
               {this.props.name}
             </a>
           </h4>
-          <span>Updated at {this.props.updated}</span>
+          <span>Updated at {this.renderDate(this.props.updated)}</span>
         </div>
         <div className="repository-info">
           <div className="repository-lang">
-            Language: {this.props.language ? this.renderLanguage() : 'not specified'}
+            Language:{' '}
+            {this.props.language ? this.renderLanguage(this.props.language) : 'not specified'}
           </div>
           <div>Stars: {this.props.stars}</div>
         </div>
